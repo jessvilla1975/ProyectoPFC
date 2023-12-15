38a84633edeaa1a657a39dfee028c35448ed7e14
ManuelVidales22 <manuelvidales33@gmail.com>
2023-12-14 21:56:58 -0500

    ingenuoMejorado

--- src/main/scala/oraculo.scala
+++ src/main/scala/oraculo.scala
@@ -22,18 +22,40 @@ object oraculo {
     auxiliar(Seq(), n)
   }
 
-  def main(args: Array[String]): Unit = {
+  // Nuevo método mejorarIngenuo
+  // Método mejorarIngenuo
+  def mejorarIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+    def auxiliar(secuencia: Seq[Char], n: Int): Seq[Char] = {
+      if (n == 0) {
+        secuencia
+      } else {
+        val mitad = secuencia.length / 2
+        val s1 = auxiliar(secuencia.take(mitad), mitad)
+        val s2 = auxiliar(secuencia.drop(mitad), n - mitad - 1)
+        // Preguntar al oráculo si s1 y s2 son subcadenas de la secuencia
+        //llamar al metodo reconstrutirCadenaIngenuo con s1 y s2
+        val s3 = reconstruirCadenaIngenuo(n, o)
+        //concatenar s1, s2 y s3
+        s1 ++ s2 ++ s3
 
-    val oraculo: Oraculo = (s: Seq[Char]) => s == Seq('a', 'x', 'b', 'a')
+      }
+    }
 
-
-    // Probamos la función con n = 4 y nuestro oráculo
-    val resultado = reconstruirCadenaIngenuo(4, oraculo)
-    // Imprimimos el resultado
-    println(resultado)
+    auxiliar(Seq(), n)
   }
 
 
+  def main(args: Array[String]): Unit = {
+    val oraculo: Oraculo = (s: Seq[Char]) => s == Seq('a', 'g', 't', 'a')
 
+    // Probamos la función reconstruirCadenaIngenuo con n = 4 y nuestro oráculo
+    val resultadoIngenuo = reconstruirCadenaIngenuo(4, oraculo)
+    // Imprimimos el resultado
+    println("Resultado Ingenuo: " + resultadoIngenuo)
 
+    // Probamos la función mejorarIngenuo con n = 4 y nuestro oráculo
+    val resultadoMejorado = mejorarIngenuo(4, oraculo)
+    // Imprimimos el resultado
+    println("Resultado Mejorado: " + resultadoMejorado)
+  }
 }
