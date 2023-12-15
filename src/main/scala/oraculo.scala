774dd31e0d015f5c3104717fca40186c808ca98e
ManuelVidales22 <manuelvidales33@gmail.com>
2023-12-14 23:24:50 -0500

    mejorado

--- src/main/scala/oraculo.scala
+++ src/main/scala/oraculo.scala
@@ -1,4 +1,3 @@
-
 import common._
 import scala.collection.parallel.CollectionConverters._
 import org.scalameter.measure
@@ -22,22 +21,22 @@ object oraculo {
     auxiliar(Seq(), n)
   }
 
+  def SubSecuencias(secuencia: Seq[Char]): (Seq[Char], Seq[Char]) = {
+    val mitad = secuencia.length / 2
+    (secuencia.take(mitad), secuencia.drop(mitad))
+  }
+
   // Nuevo método mejorarIngenuo
   // Método mejorarIngenuo
-  def mejorarIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+  def ReconstruirCadenaMejorao(n: Int, o: Oraculo): Seq[Char] = {
     def auxiliar(secuencia: Seq[Char], n: Int): Seq[Char] = {
       if (n == 0) {
         secuencia
       } else {
-        val mitad = secuencia.length / 2
-        val s1 = auxiliar(secuencia.take(mitad), mitad)
-        val s2 = auxiliar(secuencia.drop(mitad), n - mitad - 1)
-        // Preguntar al oráculo si s1 y s2 son subcadenas de la secuencia
-        //llamar al metodo reconstrutirCadenaIngenuo con s1 y s2
-        val s3 = reconstruirCadenaIngenuo(n, o)
-        //concatenar s1, s2 y s3
-        s1 ++ s2 ++ s3
-
+        val (izq, der) = SubSecuencias(secuencia)
+        val izq2 = reconstruirCadenaIngenuo(n/2, izq => o(izq))
+        val der2 = reconstruirCadenaIngenuo(n, der => o(der))
+        izq2 ++ der2
       }
     }
 
@@ -46,7 +45,7 @@ object oraculo {
 
 
   def main(args: Array[String]): Unit = {
-    val oraculo: Oraculo = (s: Seq[Char]) => s == Seq('a', 'g', 't', 'a')
+    val oraculo: Oraculo = (s: Seq[Char]) => s == Seq('a', 'g', 't', 'c')
 
     // Probamos la función reconstruirCadenaIngenuo con n = 4 y nuestro oráculo
     val resultadoIngenuo = reconstruirCadenaIngenuo(4, oraculo)
@@ -54,8 +53,10 @@ object oraculo {
     println("Resultado Ingenuo: " + resultadoIngenuo)
 
     // Probamos la función mejorarIngenuo con n = 4 y nuestro oráculo
-    val resultadoMejorado = mejorarIngenuo(4, oraculo)
+    val resultadoMejorado =  ReconstruirCadenaMejorao(4, oraculo)
     // Imprimimos el resultado
     println("Resultado Mejorado: " + resultadoMejorado)
+
+
   }
 }
